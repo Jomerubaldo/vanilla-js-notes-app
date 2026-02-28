92fd511f2044e8fcdcf2f7bdb83958a2d05b631f
Your Name <jomerubaldo@gmail.com>
2026-03-01 00:31:31 +0800

    progress today

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 
 🚀 Features & Implementation
 
+✅ 1. Create an button to add the notes and when button is click there an modal pop up to fill out
+and after that it display to the UI.
+
+✅ 2. Implement an when user type there title and paragraph for there note we can get it using .value and make an for some reason i console it to test if the value or user typing can i get.
+
+✅ 3. Implement and proper way to create name for ID and CLASS selector and for DOM javascript. for ID its for DOM javascript, and also for css its an CLASS selector
+
 🐞 Issues Encountered & Fixes ✅ 1.
 
 Issue:
--- function.js
+++ function.js
@@ -1,8 +1,14 @@
+const noteTitleValue = document.querySelector('#modalInput');
+const noteParagraphValue = document.querySelector('#textArea');
+const noteTimeElement = document.querySelector('#noteTime');
+const noteParagraphElement = document.querySelector('#noteParagraph');
+const noteTitleElement = document.querySelector('#noteTitle');
 const addNoteBtn = document.querySelector('#addButton');
 const editModal = document.querySelector('#editModal');
 const cancelEdit = document.querySelector('#cancelEdit');
 const saveEdit = document.querySelector('#saveEdit');
 
+// add
 addNoteBtn.addEventListener('click', addFunction);
 
 function addFunction() {
@@ -11,14 +17,25 @@ function addFunction() {
   document.body.classList.add('modal-open');
 }
 
+// save
 saveEdit.addEventListener('click', saveFunction);
 
 function saveFunction() {
+  // get user value
+  const title = noteTitleValue.value.trim();
+  console.log(title);
+
+  const paragraph = noteParagraphValue.value.trim();
+  console.log(paragraph);
+
+  // gagawa na dito ng logic para ma display yung input ni user sa UI
+
   // remove prevent background scrolling so back to normal when modal is close
   document.body.classList.remove('modal-open');
   editModal.style.display = 'none';
 }
 
+// cancel
 cancelEdit.addEventListener('click', cancelFunction);
 
 function cancelFunction() {
