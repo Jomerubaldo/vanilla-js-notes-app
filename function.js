831330b9515eeaa8c99ab920e269effb16a46ecc
Your Name <jomerubaldo@gmail.com>
2026-03-02 00:17:02 +0800

    progress today

--- function.js
+++ function.js
@@ -1,14 +1,21 @@
+const countTask = document.querySelector('#countTask');
 const noteTitleValue = document.querySelector('#modalInput');
-const noteParagraphValue = document.querySelector('#textArea');
-const noteTimeElement = document.querySelector('#noteTime');
-const noteParagraphElement = document.querySelector('#noteParagraph');
-const noteTitleElement = document.querySelector('#noteTitle');
+const noteTextAreaValue = document.querySelector('#textArea');
+// const noteTitleElement = document.querySelector('#noteTitle');
+// const noteParagraphElement = document.querySelector('#noteParagraph');
+// const noteTimeElement = document.querySelector('#noteTime');
 const addNoteBtn = document.querySelector('#addButton');
 const editModal = document.querySelector('#editModal');
 const cancelEdit = document.querySelector('#cancelEdit');
-const saveEdit = document.querySelector('#saveEdit');
+const saveNotes = document.querySelector('#saveNotes');
 
-// add
+// save to localStorage
+// const notes = JSON.parse(localStorage.setItem('notes')) || [];
+
+// function saveLocalStorageNotes() {
+//   localStorage.setItem('notes', JSON.stringify(notes));
+// }
+
 addNoteBtn.addEventListener('click', addFunction);
 
 function addFunction() {
@@ -17,18 +24,42 @@ function addFunction() {
   document.body.classList.add('modal-open');
 }
 
-// save
-saveEdit.addEventListener('click', saveFunction);
+saveNotes.addEventListener('click', saveFunction);
+
+const notes = [];
 
 function saveFunction() {
-  // get user value
-  const title = noteTitleValue.value.trim();
-  console.log(title);
+  const title = noteTitleValue.value;
+  const textArea = noteTextAreaValue.value;
 
-  const paragraph = noteParagraphValue.value.trim();
-  console.log(paragraph);
+  notes.push({
+    text: title,
+    content: textArea,
+  });
 
-  // gagawa na dito ng logic para ma display yung input ni user sa UI
+  countTask.textContent = notes.length;
+
+  notes.forEach((note) => {
+    const noteTitleElement = document.createElement('h3');
+    noteTitleElement.className = 'note-title';
+    noteTitleElement.textContent = note.text; // very important to render a forEach
+
+    const noteTextAreaElement = document.createElement('p');
+    noteTextAreaElement.className = 'note-text-area';
+    noteTextAreaElement.textContent = note.content; // very important to render a forEach
+
+    // const noteTimeElement = document.createElement('p');
+    // noteTimeElement.className = 'note-time';
+
+    const containerNote = document.querySelector('#containerNote');
+
+    containerNote.appendChild(noteTitleElement);
+    containerNote.appendChild(noteTextAreaElement);
+    // containerNote.appendChild(noteTimeElement);
+  });
+
+  noteTitleValue.value = '';
+  noteTextAreaValue.value = '';
 
   // remove prevent background scrolling so back to normal when modal is close
   document.body.classList.remove('modal-open');
