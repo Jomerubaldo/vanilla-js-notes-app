96348ff9f9535fd562ef5c0ae7b91677eed9f856
Your Name <jomerubaldo@gmail.com>
2026-03-09 05:45:31 +0800

    implement closingModal button

--- function.js
+++ function.js
@@ -7,68 +7,33 @@ const editModal = document.querySelector('#editModal');
 const cancelEdit = document.querySelector('#cancelEdit');
 const saveAddNotes = document.querySelector('#saveAddNotes');
 const containerNote = document.querySelector('#containerNote');
+const closeViewModal = document.querySelector('#closeViewModal');
+const headerCloseModal = document.querySelector('#headerCloseModal');
 
-// save to localStorage
-// const notes = JSON.parse(localStorage.setItem('notes')) || [];
+// STATE (DATA)
+let notes = JSON.parse(localStorage.getItem('notes')) || [];
 
-// function saveLocalStorageNotes() {
-//   localStorage.setItem('notes', JSON.stringify(notes));
-// }
-
-// STATE SECTION
-const notes = [];
-
-// EVENT SECTION
-addNoteBtn.addEventListener('click', addFunction);
-saveAddNotes.addEventListener('click', saveAddFunction);
-cancelEdit.addEventListener('click', cancelFunction);
-
-// LOGIC SECTION
-function addFunction() {
-  editModal.style.display = 'flex'; // show modal
-  // prevent background scrolling while open modal
-  document.body.classList.add('modal-open');
+// STORAGE
+function saveNotes() {
+  localStorage.setItem('notes', JSON.stringify(notes));
 }
 
-// reusable close modal
-function closeModal() {
-  document.body.classList.remove('modal-open');
-  editModal.style.display = 'none';
-}
-
-function saveAddFunction() {
-  const title = noteTitleValue.value;
-  const textArea = noteTextAreaValue.value;
-
-  if (!title.trim() || !textArea) {
-    let message = '';
-
-    if (!title.trim() && !textArea) {
-      message = 'Please enter both a title and a note fields';
-    } else if (!title.trim()) {
-      message = 'Please enter a title';
-    } else {
-      message = 'Please enter a note';
-    }
-
-    Swal.fire({
-      icon: 'warning',
-      title: 'Oops...',
-      text: message, //called variable
-    });
-
-    return; //last call return to back the logic dalidation at a top
-  }
-
-  notes.push({
-    text: title,
-    content: textArea,
-  });
-
-  // countTask.textContent = notes.length;
-
+// UI FUNCTIONS
+function createNote(title, textArea) {
   const contentNote = document.createElement('div');
   contentNote.className = 'content-note';
+
+  contentNote.addEventListener('click', function () {
+    const viewModalContainer = document.querySelector('#viewModalContainer');
+    const viewTitle = document.querySelector('#viewNoteTitle');
+    const viewParagraph = document.querySelector('#viewNoteParagraph');
+
+    viewTitle.textContent = title;
+    viewParagraph.textContent = textArea;
+
+    viewModalContainer.style.display = 'flex';
+    document.body.classList.add('modal-open');
+  });
 
   const noteTitleElement = document.createElement('h3');
   noteTitleElement.className = 'note-title';
@@ -81,47 +46,85 @@ function saveAddFunction() {
   const horizontalElement = document.createElement('hr');
   horizontalElement.className = 'horizontal-line';
 
-  contentNote.addEventListener('click', function () {
-    // DOM FOR VIEW-MODAL-NOTES
-    const viewModalContainer = document.querySelector('#viewModalContainer');
-    const viewTitle = document.querySelector('#viewNoteTitle');
-    const viewParagraph = document.querySelector('#viewNoteParagraph');
-
-    viewTitle.textContent = title;
-    viewParagraph.textContent = textArea;
-
-    viewModalContainer.style.display = 'flex';
-    document.body.classList.add('modal-open');
-  });
-
-  // close modal in view note
-  const closeViewModal = document.querySelector('#closeViewModal');
-
-  closeViewModal.addEventListener('click', function () {
-    document.body.classList.remove('modal-open');
-    viewModalContainer.style.display = 'none';
-  });
-
-  lucide.createIcons();
-
-  // const noteTimeElement = document.createElement('p');
-  // noteTimeElement.className = 'note-time';
-
-  // building structure
   contentNote.appendChild(noteTitleElement);
   contentNote.appendChild(noteTextAreaElement);
   contentNote.appendChild(horizontalElement);
-  // containerNote.appendChild(noteTimeElement);
+
   containerNote.appendChild(contentNote);
+}
+
+// render saved notes
+function renderNotes() {
+  notes.forEach((note) => {
+    createNote(note.text, note.content);
+  });
+}
+
+// MODAL FUNCTIONS
+function openModal() {
+  editModal.style.display = 'flex';
+  document.body.classList.add('modal-open');
+}
+
+function closeModal() {
+  document.body.classList.remove('modal-open');
+  editModal.style.display = 'none';
+}
+
+function closeModalView() {
+  document.body.classList.remove('modal-open');
+  viewModalContainer.style.display = 'none';
+}
+
+// LOGIC
+function addNote() {
+  const title = noteTitleValue.value;
+  const textArea = noteTextAreaValue.value;
+
+  if (!title.trim() || !textArea) {
+    let message = '';
+
+    if (!title.trim() && !textArea) {
+      message = 'Please fill both Title and Note fields';
+    } else if (!title.trim()) {
+      message = 'Please fill Title';
+    } else {
+      message = 'Please fill Note';
+    }
+
+    Swal.fire({
+      icon: 'warning',
+      title: 'Oops...',
+      text: message,
+    });
+
+    return;
+  }
+
+  const newNote = {
+    text: title,
+    content: textArea,
+  };
+
+  notes.push(newNote);
+
+  saveNotes();
+
+  createNote(title, textArea);
 
   noteTitleValue.value = '';
   noteTextAreaValue.value = '';
 
   closeModal();
 }
+lucide.createIcons();
 
-function cancelFunction() {
-  closeModal();
-}
+// EVENTS
+addNoteBtn.addEventListener('click', openModal);
+saveAddNotes.addEventListener('click', addNote);
+cancelEdit.addEventListener('click', closeModal);
+closeViewModal.addEventListener('click', closeModalView);
+headerCloseModal.addEventListener('click', closeModal);
 
-//need refactor to clean code DRY principle
+// INIT APP
+renderNotes();
